d966904067d4a0aea26f262b17a8929e60b67274
olllechka <113857825+olllechka@users.noreply.github.com>
2023-01-20 14:51:48 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 user_token = 'vk1.a.8pvOWbVMskR_aLxC7Qqym_j42XjlDVtjQz9rng8x0FZ2RetP1dOTme0Sj3ujdiw_98MRCN0mdDTZP-H-FVT8mKm2jGKOiO6anP4ItYqo2A6Xsg0JdFqv2D3pSrVQTyvcg27LBeRM-Q_WYFwhgSTqOvQo8jEyJOtBGyRa6Ud7qhuDmt4M1bpxu6YeRxUZpYbhS3Fyqn2LM8RCZcICNEm5gg' # токен пользователя
-comm_token = 'vk1.a.CIK-WPS4O_Bb7FtVaPbV_yPq9kqhGWfFqhOdjeFx8zNONuXtOE8x72BeGdm7FLro-Z11ti0k79JQ6OtlaFgX1jrLpXYem4ZsIglcbxnlPZJ8Y9BLgiktWguF3kG-6_lHih-bBsWvaDW6oqVch_2CXSfsRB7Am7Ih6Fyj8yHz6413SI5X5F_8aZdhHRIrXf5CJv0C4GZKjwn1o4yDxydoTw' # токен сообщества
+comm_token = '' # токен сообщества
 
 offset = 0 #сдвиг
 line = range(0, 1000) # последовательность для перебора найденных пользователей
