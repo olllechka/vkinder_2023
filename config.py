5fc9935c11ca46fbc374c0b52d8b273aa022a926
olllechka <113857825+olllechka@users.noreply.github.com>
2023-02-01 13:05:17 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
-user_token = 'vk1.a.8pvOWbVMskR_aLxC7Qqym_j42XjlDVtjQz9rng8x0FZ2RetP1dOTme0Sj3ujdiw_98MRCN0mdDTZP-H-FVT8mKm2jGKOiO6anP4ItYqo2A6Xsg0JdFqv2D3pSrVQTyvcg27LBeRM-Q_WYFwhgSTqOvQo8jEyJOtBGyRa6Ud7qhuDmt4M1bpxu6YeRxUZpYbhS3Fyqn2LM8RCZcICNEm5gg' # токен пользователя
+user_token = '' # токен пользователя
 comm_token = '' # токен сообщества
 
 offset = 0 #сдвиг
